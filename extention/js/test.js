ad473d1dd398b3aab74d79d3f9072d4ce1c3e325
Danylo Soloviov <41167337+Denend@users.noreply.github.com>
2019-03-10 06:51:16 +0800

    All works but there is a mistake  while passing a param to calculateErrValue function, should do something to URL.

--- extention/js/test.js
+++ extention/js/test.js
@@ -2,37 +2,56 @@ console.log("we are ready");
 console.log("we are ready");
 var currLocation = window.location.href;
 
-//document.addEventListener('DOMContentLoaded', function(){
-	//checkErrNumber();
-	if(currLocation.includes("tcs.jiyunhudong.com/worktable/6580176409300304392")){
-		alert("some are works")
-		document.getElementsByClassName("action-bar-button ivu-btn ivu-btn-primary")[0].addEventListener("click", checkErrNumber());
-	}
-//})
+  document.onkeyup = function(e){
+     if(e.keyCode==32){
+  		setTimeout(function(){
+			calculateErrValue('tcs.jiyunhudong.com/worktable/6575057855890588173').then(function(res){
+				console.log(res)
+				let previousVal = sessionStorage.getItem('errValue');
+				if(res == previousVal){
+					alert("someone did a mistake")
+				}
+			});
+		
+		},3000);
+
+     };
+  }
+
+
+
+
+calculateErrValue('tcs.jiyunhudong.com/worktable?tags=&page=1&title=&type=star').then(function(res){
+console.log(res)
+sessionStorage.setItem('errValue', res);
+})
 
 
 
 
 
-if(currLocation.includes("tcs.jiyunhudong.com")){
-let mistakesNum = "";
-let audioQueue = 6575057855890588173
-let notes = ",6580176409300304392"
-fetch("https://tcs.jiyunhudong.com/api/v2/stats/common_stats/?project_ids=6575057855890588173&username=danylo.soloviov.bd.pl@gmail.com&extra_stats=delay_stats", {"credentials":"include","headers":{"accept":"application/json, text/plain, */*","accept-language":"en-US,en;q=0.9","x-requested-with":"XMLHttpRequest","x-ts":"1551428308072"},"referrer":"https://tcs.jiyunhudong.com/worktable?tags=&page=1&title=&type=star","referrerPolicy":"no-referrer-when-downgrade","body":null,"method":"GET","mode":"cors"}).then(
-function(res){
-	res.json().then(
-		function(res1){
-			console.log(res1)
-			mistakesNum = res1.data;
-			let parsedObj = JSON.stringify(mistakesNum);
-			console.log(parsedObj)
+
+function calculateErrValue(url){
+	if(currLocation.includes(url)){
+	let mistakesNum = "";
+	let audioQueue = 6575057855890588173
+	let notes = ",6580176409300304392"
+	return fetch("https://tcs.jiyunhudong.com/api/v2/stats/common_stats/?project_ids=6575057855890588173&username=danylo.soloviov.bd.pl@gmail.com&extra_stats=delay_stats", {"credentials":"include","headers":{"accept":"application/json, text/plain, */*","accept-language":"en-US,en;q=0.9","x-requested-with":"XMLHttpRequest","x-ts":"1551428308072"},"referrer":"https://tcs.jiyunhudong.com/worktable?tags=&page=1&title=&type=star","referrerPolicy":"no-referrer-when-downgrade","body":null,"method":"GET","mode":"cors"}).then(
+	function(res){
+		return res.json().then(
+			 function(res1){
+				console.log(res1)
+				mistakesNum = res1.data;
+				let parsedObj = JSON.stringify(mistakesNum);
+				console.log(parsedObj)
 			
-			let position = fourthIndex(parsedObj,":")
-			console.log(position)
-			let errVal = parsedObj.slice(position +1,position+2);
-			console.log(errVal)
-			sessionStorage.setItem('errValue', errVal);
-	})})
+				let position = fourthIndex(parsedObj,":")
+				console.log(position)
+				let errVal = parsedObj.slice(position +1,position+2);
+				return errVal
+				//console.log(errVal)				
+		})})
+	};
 };
 
 function fourthIndex(text, symbol) {
